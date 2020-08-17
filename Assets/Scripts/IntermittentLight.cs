500205eef85b8576681abff3d7366c367d5cab52
JustinGalante1 <jn2wf@virginia.edu>
2020-08-17 05:29:40 -0400

    Added wall lights, fixed menus, added interesting lighting

---
+++ Assets/Scripts/IntermittentLight.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntermittentLight : MonoBehaviour
+{
+    private Light myLight;
+    private bool goingUp;
+
+    private void Start()
+    {
+        myLight = this.GetComponent<Light>();
+        goingUp = false;
+    }
+
+    void Update()
+    {
+        if (goingUp)
+        {
+            if (myLight.range < 10)
+            {
+                myLight.range += .03f;
+            }
+            else
+            {
+                goingUp = false;
+            }
+        }
+        else
+        {
+            if(myLight.range > 3)
+            {
+                myLight.range -= .03f;
+            }
+            else
+            {
+                goingUp = true;
+            }
+        }
+    }
+}
