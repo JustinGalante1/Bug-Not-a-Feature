624d78d05fd0459a5018a8adcde50f4e0d5a07fc
JustinGalante1 <jn2wf@virginia.edu>
2020-08-23 15:22:25 -0400

    Added a start menu and made sure everything is ready for itch.io

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public float panAmount;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, panAmount * Time.deltaTime, 0, Space.World);
+    }
+}
---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject infoPanel;
+
+    private void Start()
+    {
+        infoPanel.SetActive(false);
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void showInfo()
+    {
+        this.gameObject.SetActive(false);
+        infoPanel.SetActive(true);
+
+    }
+
+    public void hideInfo()
+    {
+        this.gameObject.SetActive(true);
+        infoPanel.SetActive(false);
+    }
+}
